a3903f36a028fb779ca6cdee5e0b9c3509aa2180
Gianpaolo Macario <gmacario@gmail.com>
2016-01-12 14:54:05 +0100

    Configure VM name via enviroment variable
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- runme.sh
+++ runme.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-MACHINE=easy-jenkins
+[[ "${VM}" = "" ]] && VM=easy-jenkins
 
 set -e
 
 docker-machine ls
 
-if docker-machine ls | grep ${MACHINE} >/dev/null; then
-    echo "WARNING: Docker machine ${MACHINE} exists, skipping docker-machine create"
+if docker-machine ls | grep ${VM} >/dev/null; then
+    echo "WARNING: Docker machine ${VM} exists, skipping docker-machine create"
 else
     docker-machine create --driver virtualbox \
       --virtualbox-cpu-count "2" \
       --virtualbox-disk-size "50000" \
       --virtualbox-memory "3048" \
-      ${MACHINE}
+      ${VM}
 fi
 
-docker-machine env ${MACHINE}
+docker-machine env ${VM}
 
-eval $(docker-machine env ${MACHINE})
+eval $(docker-machine env ${VM})
 docker-compose up -d
 
 # EOF
