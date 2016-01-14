99278c2532dd1c3c010aa03e6323bed26af01fcd
Gianpaolo Macario <gmacario@gmail.com>
2016-01-14 10:32:39 +0100

    myscripts: Add sysinfo.groovy
    Simple Groovy script to print some system information
    on the node running Jenkins.
    
    Limitations: Assumes to run on a POSIX-like environment (i.e. Linux + bash)
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

---
+++ myscripts/sysinfo.groovy
@@ -0,0 +1,31 @@
+/*
+  sysinfo.groovy
+
+  Assumes to run on a POSIX-like environment (i.e. Linux + bash)
+
+  See https://learnxinyminutes.com/docs/groovy/
+*/
+
+println "INFO: sysinfo.groovy"
+println ""
+
+println "INFO: Executing uname -a"
+println "uname -a".execute().text
+
+println "INFO: Executing id"
+println "id".execute().text
+
+println "INFO: Inspecting PATH"
+println System.getenv("PATH")
+println ""
+
+println "INFO: Executing df -h"
+println "df -h".execute().text
+
+//println "INFO: Executing sudo id"
+//println "sudo id".execute().text // java.io.IOException: Cannot run program "sudo": error=2, No such file or directory
+
+println "INFO: Executing df -h"
+println "printenv".execute().text
+
+/* EOF */
