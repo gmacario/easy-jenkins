fd8bc0c308c2bd2a75c13f7e92059f0a9b9cbb29
Gianpaolo Macario <gmacario@gmail.com>
2016-01-17 14:31:01 +0100

    Create seed-configure-git.groovy

---
+++ mydsl/seed-configure-git.groovy
@@ -0,0 +1,47 @@
+/* 
+Usage:
+
+Browse ${JENKINS_URL}/view/All/newJob
+
+* Item Name: seed-build-gdp
+* Type: Freestyle project
+
+Browse ${JENKINS_URL}/job/seed-build-gdp/configure
+
+* Restrict where this project can be run
+  - Label Expression: master
+  
+* Build > Add build step
+  - Process Job DSLs
+  - DSL Script: <paste those contents>
+  
+Browse ${JENKINS_URL}/job/seed-build-gdp/build?delay=0sec
+
+*/
+
+def jobName = "configure-git"
+
+job(jobName) {
+  
+  // Warning: (script, line 26) plugin 'nodelabelparameter' needs to be installed
+  /* parameters {
+        nodeParam('build-yocto-slave') {
+            description('select test host')
+            defaultNodes(['node1'])
+            allowedNodes(['node1', 'node2', 'node3'])
+            trigger('multiSelectionDisallowed')
+            eligibility('IgnoreOfflineNodeEligibility')
+        }
+  } */
+    
+  steps {
+    shell "id"
+    shell "printenv"
+    shell "ps axf"
+    shell "git config -l"
+    shell "echo \$(whoami)"
+    shell "echo \$(hostname)"
+    shell "git config --global user.name \"\$(whoami)\""
+    shell "git config --global user.email \"\$(whoami)@\$(hostname)\""
+  }
+}
