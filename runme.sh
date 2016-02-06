217f050c3f8798ed5b0d67ba193858e8c7223e22
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2016-02-06 11:48:40 +0100

    Check versions for required programs
    - docker
    - docker-compose
    - docker-machine
    
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- runme.sh
+++ runme.sh
@@ -14,6 +14,55 @@
 #       ./runme.sh
 # =============================================================================
 
+# -----------------------------------------------------------------------------
+# Check version "maj.min.pat"
+#
+# Parameters:
+#   * have_maj_min_pat: Installed version - example: "0.5.6"
+#   * want_maj_min_pat: Required version  - example: "0.6.0"
+#
+# Return:
+#   * 0 if installed version is >= than required version
+#   * 1 otherwise
+#
+is_version_ok () {
+  result=1
+  # echo "DEBUG: line $LINENO: is_version_ok(have_maj_min_pat=$1, want_maj_min_pat=$2)"
+
+  have_maj=$(echo $1 | sed -e 's/\..*//')
+  have_min_pat=$(echo $1 | sed -e 's/^[0-9]*\.//')
+  have_min=$(echo $have_min_pat | sed -e 's/\..*//')
+  have_pat=$(echo $have_min_pat | sed -e 's/^[0-9]*\.//')
+  # echo "DEBUG: line $LINENO: have_maj=$have_maj, have_min=$have_min, have_pat=$have_pat"
+
+  want_maj=$(echo $2 | sed -e 's/\..*//')
+  want_min_pat=$(echo $2 | sed -e 's/^[0-9]*\.//')
+  want_min=$(echo $want_min_pat | sed -e 's/\..*//')
+  want_pat=$(echo $want_min_pat | sed -e 's/^[0-9]*\.//')
+  # echo "DEBUG: line $LINENO: want_maj=$want_maj, want_min=$have_min, want_pat=$want_pat"
+
+  if [ $have_maj -gt $want_maj ]; then
+    # echo "DEBUG: line $LINENO: Case have_maj $have_maj > $want_maj ==> OK"
+    result=0
+  elif [ $have_maj -eq $want_maj ]; then
+    # echo "DEBUG: line $LINENO: Case have_maj $have_maj == $want_maj"
+    if [ $have_min -gt $want_min ]; then
+      # echo "DEBUG: line $LINENO: Case have_min $have_min > $want_min ==> OK"
+      result=0
+    elif [ $have_min -eq $want_min ]; then
+      # echo "DEBUG: line $LINENO: Case have_min $have_min == 5"
+      if [ $have_pat -ge $want_pat ]; then
+        # echo "DEBUG: line $LINENO: Case have_pat $have_pat >= $want_pat ==> OK"
+        result=0
+      fi
+    fi
+  fi
+  # echo "DEBUG: line $LINENO: Return result=$result"
+  return $result
+}
+
+# -----------------------------------------------------------------------------
+
 set -e
 
 if [[ "${VM}" = "" ]]; then
@@ -24,6 +73,56 @@ fi
 [[ "${VM_NUM_CPUS}" = "" ]] && VM_NUM_CPUS=2
 [[ "${VM_MEM_SIZEMB}" = "" ]] && VM_MEM_SIZEMB=2048
 [[ "${VM_DISK_SIZEMB}" = "" ]] && VM_DISK_SIZEMB=50000
+
+# Check prerequisites
+
+# docker
+if ! which docker >/dev/null; then
+    echo "ERROR: Cannot find docker"
+    exit 1
+fi
+result=$(docker --version)
+echo "DEBUG: line $LINENO: result=$result"
+have_maj_min_pat=$(echo $result | sed -e 's/^.*version //' | sed -e 's/\,.*$//')
+# echo "DEBUG: line $LINENO: have_maj_min_pat=$have_maj_min_pat"
+want_maj_min_pat="1.9.0"    # TODO: "1.10.0"
+# echo "DEBUG: line $LINENO: want_maj_min_pat=$want_maj_min_pat"
+if ! is_version_ok $have_maj_min_pat $want_maj_min_pat; then
+    echo "ERROR: Should install docker >= $want_maj_min_pat (have $have_maj_min_pat)"
+    exit 1
+fi
+
+# docker-compose
+if ! which docker-compose >/dev/null; then
+    echo "ERROR: Cannot find docker-compose"
+    exit 1
+fi
+result=$(docker-compose --version)
+echo "DEBUG: line $LINENO: result=$result"
+have_maj_min_pat=$(echo $result | sed -e 's/^.*version //' | sed -e 's/\,.*$//')
+# echo "DEBUG: line $LINENO: have_maj_min_pat=$have_maj_min_pat"
+want_maj_min_pat="1.5.1"    # TODO: 1.6.0"
+# echo "DEBUG: line $LINENO: want_maj_min_pat=$want_maj_min_pat"
+if ! is_version_ok $have_maj_min_pat $want_maj_min_pat; then
+    echo "ERROR: Should install docker-compose >= $want_maj_min_pat (have $have_maj_min_pat)"
+    exit 1
+fi
+
+# docker-machine
+if ! which docker-machine >/dev/null; then
+  echo "ERROR: Cannot find docker-machine"
+  exit 1
+fi
+result=$(docker-machine --version)
+echo "DEBUG: line $LINENO: result=$result"
+have_maj_min_pat=$(echo $result | sed -e 's/^.*version //' | sed -e 's/\,.*$//')
+# echo "DEBUG: line $LINENO: have_maj_min_pat=$have_maj_min_pat"
+want_maj_min_pat="0.5.3"   # TODO: "0.6.0"
+# echo "DEBUG: line $LINENO: want_maj_min_pat=$want_maj_min_pat"
+if ! is_version_ok $have_maj_min_pat $want_maj_min_pat; then
+  echo "ERROR: Should install docker-machine >= $want_maj_min_pat (have $have_maj_min_pat)"
+  exit 1
+fi
 
 # docker-machine ls
 if docker-machine ls | grep -w ${VM} >/dev/null; then
