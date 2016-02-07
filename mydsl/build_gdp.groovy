9235a6b40619242c2eea92a88aaeeffca12eeb90
Gianpaolo Macario <gmacario@gmail.com>
2016-02-07 10:05:53 +0100

    Fixups when renaming to build_gdp.groovy

--- mydsl/build_gdp.groovy
+++ mydsl/build_gdp.groovy
@@ -1,26 +1,30 @@
 /* 
   Project: https://github.com/gmacario/easy-jenkins
-  File:    mydsl/seed_build_gdp.groovy
+  File:    mydsl/build_gdp.groovy
 */
   
 /*
 Usage:
 
-Browse `${JENKINS_URL}/view/All/newJob`
+Browse `${JENKINS_URL}/view/All/newJob`, then click **New Item**
 
-* Item Name: seed_build_gdp
+* Item Name: `build_gdp`
 * Type: Freestyle project
 
-Browse `${JENKINS_URL}/job/seed-build-gdp/configure`
+then click **OK**.
 
-* Restrict where this project can be run
-  - Label Expression: master
+Browse `${JENKINS_URL}/job/build_gdp/configure`
+
+* Restrict where this project can be run: Yes
+  - Label Expression: `yocto`
   
 * Build > Add build step
   - Process Job DSLs
-  - DSL Script: <paste those contents>
+  - DSL Script: <paste these file>
   
-Browse `${JENKINS_URL}/job/seed_build_gdp/build?delay=0sec`
+then click **Save**.
+
+Browse `${JENKINS_URL}/job/build_gdp/build?delay=0sec`
 */
 
 def jobName = "build_gdp"
@@ -43,3 +47,5 @@ job(jobName) {
     shell "bash -xec \"source init.sh && bitbake genivi-demo-platform\""
   }
 }
+
+// EOF
