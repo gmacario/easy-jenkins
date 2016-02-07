44e7e7e9c7c979ae5d7579176923b185de031a3a
Gianpaolo Macario <gmacario@gmail.com>
2016-02-07 22:17:26 +0100

    Rename param to AgentLabels, improve descriptions

--- mydsl/seed_add_jenkins_slave.groovy
+++ mydsl/seed_add_jenkins_slave.groovy
@@ -11,11 +11,11 @@ job('add_jenkins_slave') {
     git('https://github.com/gmacario/easy-jenkins', '*/master')
     
     parameters {
-      textParam('AgentList', 'build-yocto-slave', 'Name of agents to create, optionally more than one (each line makes one agent)')
+      textParam('AgentList', 'build-yocto-slave', 'Names of the agent(s) to create (each line makes one agent)')
       stringParam('AgentDescription', 'Auto-created Jenkins agent', 'Description that will be set for _every_ created agent')
       stringParam('AgentExecutors', '2', 'Number of executors for the agent')
       stringParam('AgentHome', '/home/jenkins', 'Remote filesystem root for the agent')
-      textParam('AgentLabel', 'yocto', 'Labels associated to this agent (each line makes one label)')
+      textParam('AgentLabels', 'yocto', 'Labels associated to the agent (each line makes one label)')
     }
     
     steps {
