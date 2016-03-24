0d3783ec3654bb0d97ef8aa67db57670049cd47c
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2016-03-24 09:30:42 +0100

    myjenkins: Use Docker daemon from host
    Instead than running a separate Docker daemon,
    run service myjenkins bind mounting host .
    
    See https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
    
    Fix https://github.com/gmacario/easy-jenkins/issues/31
    
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- myjenkins/start.sh
+++ myjenkins/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 sleep 5
-service docker start
+
+# To use host Docker daemon, comment out line `service docker start`
+# and run container with `--volume=/var/run/docker.sock:/var/run/docker.sock`
+# service docker start
+
 gosu jenkins /usr/local/bin/jenkins.sh
 
 # EOF
